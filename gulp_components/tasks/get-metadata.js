97eed57cd10b86f5ee08202518aeb77fdb7d56bb
sam.elwitt <sam.elwitt@ogilvy.com>
2015-08-26 10:24:48 -0400

    fix sction_only logic

--- gulp_components/tasks/get-metadata.js
+++ gulp_components/tasks/get-metadata.js
@@ -41,7 +41,7 @@ gulp.task('getMetaData', function(done){
 
 		if (item.children && item.children.length > 0) {
 			for (var i = 0; i < item.children.length; i++) {
-				if (!item.children[i].section_only || item.children[i].section_only !== 'true') {
+				if (typeof item.children[i].section_only === 'undefined' || item.children[i].section_only === 'false') {
 					PAGES.push(item.children[i].url);
 				}
 				getPagesList(item.children[i]); //recursive for children of children
