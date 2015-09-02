e35cd26036073f23176f164e9161b8d633705958
samelwitt <samelwitt@gmail.com>
2015-09-02 14:12:38 -0400

    fix seo overwrite

--- seo.js
+++ seo.js
@@ -1,98 +1,69 @@
-/**
- * run the site in phantomjs, scrape the content, insert it into noscript tags
- */
+var system = require('system'),
+		page = require('webpage').create(),
+		fs = require('fs'),
+		config = require('../../config'),
+		dir = '_tmp-noscript/',
+		env, host, body, fileName, text, ready, t, c, i= 0, path;
 
-var gulp = require('gulp'),
-		opts = require('../opts');
+if (system.args[1] === 'stage') {
+	page.settings.userName = 'ogilvy';
+	page.settings.password = 'd1g1t@l';
+	env = 'stage';
+}
+else {
+	env = 'prod';
+}
 
-//main task in this sequence
-gulp.task('seo',function(done){
+if (system.args[2] && system.args[2].length > 0) {
+	fileName = system.args[2];
+	path = system.args[2] === '/' ? '' : '/' + system.args[2];
+}
+else {
+	console.log('[phantomjs log]' + ' ' + 'No path supplied!');
+	phantom.exit();
+}
 
-	opts.logMsg('\n*****' + 'begin seo task' + '*****\n');
+host = config.siteUrl[env];
+getHtml();
 
-	var sys = require('sys'),
-			exec = require('child_process').exec,
-			args = opts.packages.yargs.argv,
-			PAGES = ['/'],
-			command, env,
-			i= 0;
+function getHtml() {
+	//var c = 0;
 
-	getSitenav(function(data){
+	var ready = false;
 
-		getPagesList(data.items[0]);
-		opts.logMsg('starting seo scraping...');
-		env = args.stage ? 'stage' : 'prod';
-		runPhantom(PAGES[i]);
-	});
+	page.open(host + path, function() {
 
+		t = setInterval(function(){
 
-	function getSitenav(cb) {
+			ready = page.evaluate(function(){
+				return document.getElementById('layout') !== null;
+			});
 
-		opts.fs.readFile(opts.src + '/json/sitenav.json', {encoding: 'utf8'}, function(err, data){
-			if (err) throw err;
-			cb(JSON.parse(data));
-		});
-	}
+			//console.log(ready);
 
-	function getPagesList(item) {
+			if (ready) {
+				clearInterval(t);
+				body = page.evaluate(function() {
+					return document.getElementById('ibm-content-main');
+				});
+				text = body.innerHTML.replace(/<script[^>]*>([\s\S]*?)<\/script>/gm,'');
 
-		if (item.children && item.children.length > 0) {
-			for (var i = 0; i < item.children.length; i++) {
-				if (!item.children[i].section_only || item.children[i].section_only === 'false') {
-					PAGES.push(item.children[i].url);
+				if (text.length < 1) {
+					console.log('[phantomjs log]' + ' ' + 'Scrape empty!');
 				}
-				getPagesList(item.children[i]); //recursive for children of children
-			}
-		}
-	}
 
-	function runPhantom(page) {
-		opts.logMsg(process.cwd());
-		opts.logMsg('scraping ' + page + '...allowing 5s for it load...');
-		command = 'phantomjs ./bower_components/phonic-build-tasks/seo.js ' + env + ' ' + page;
-		exec(command, function(error, stdout, stderr){
-			if (stdout.indexOf('[phantomjs log]') === 0) {
-				opts.logErr('************ ERROR: ' + stdout);
+				else {
+					console.log(text);
+				}
+
+				phantom.exit();
 			}
 			else {
-				injectSeo(page, stdout);
-				i++;
-
-				if (i < PAGES.length) {
-					runPhantom(PAGES[i]);
-				}
-				else {
-					opts.logMsg('scraping complete\n');
-					done();
-				}
+				console.log('[phantomjs log]' + ' ' + fileName + ' page timeout, page not executing properly or can\'t find #layout, exiting incomplete... \n');
+				clearInterval(t);
+				phantom.exit();
 			}
-		});
-	}
 
-	function injectSeo(path, contents) {
-		var destDir;
-
-		if (path === '/') {
-			destDir = opts.dist + path;;
-		}
-		else {
-			destDir = opts.dist + '/' + path;
-		}
-
-		contents = opts.packages.htmlmin(contents, {
-			removeComments: true,
-			collapseWhitespace: true
-		});
-
-		if (contents.length < 100) {
-			opts.logErr('Content looks way too short, aborting...');
-		}
-
-		opts.logMsg('injecting seo into ' + destDir + 'index.html...');
-
-		gulp.src(destDir + 'index.html')
-				.pipe(opts.plugins.replace(/<noscript.id="seo"[^>]*>([\s\S]*?)<\/noscript>/gm,'')) //remove any previous instances of noscript block
-				.pipe(opts.plugins.injectString.before('<div id="ibm-footer">', '<noscript id="seo">'+ contents + '</noscript>\n'))
-				.pipe(gulp.dest(destDir));
-	}
-});
+		}, 5000);
+	});
+}
