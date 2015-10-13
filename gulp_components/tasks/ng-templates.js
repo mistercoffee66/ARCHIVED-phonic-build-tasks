4db1b1ad3c443bd42ecb387b10eabf803fe168bc
samelwitt <samelwitt@gmail.com>
2015-10-13 13:00:21 -0400

    clean js/templates before converting html

--- gulp_components/tasks/ng-templates.js
+++ gulp_components/tasks/ng-templates.js
@@ -3,14 +3,19 @@
  */
 
 var gulp = require('gulp'),
-	opts = require('../opts');
+		opts = require('../opts');
 
-gulp.task('ng-templates',function(){
+gulp.task('ng-templates',function(done){
 	opts.logMsg('\n*****' + 'begin ng-templates' + '*****\n');
-	return gulp.src(opts.src + opts.ngTemplates)
-			.pipe(opts.plugins.ngHtml2js({
-				moduleName: "ibmnww",
-				declareModule: false
-			}))
-			.pipe(gulp.dest(opts.src + opts.jsDir + '/templates'));
+
+	//clean out the directory then make the new templates
+	opts.fs.emptyDir(opts.src + opts.jsDir + '/templates', function(err) {
+		gulp.src(opts.src + opts.ngTemplates)
+				.pipe(opts.plugins.ngHtml2js({
+					moduleName: "ibmnww",
+					declareModule: false
+				}))
+				.pipe(gulp.dest(opts.src + opts.jsDir + '/templates'));
+		done();
+	});
 });
