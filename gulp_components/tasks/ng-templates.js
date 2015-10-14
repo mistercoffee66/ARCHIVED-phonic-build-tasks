2cb096a37fe6a7cd70d4f6a45f4d45a4819fdd66
Ogilvy Dev <ogilvydev@users.noreply.github.com>
2015-10-14 10:44:29 -0400

    comment out delete pending race condition fix

--- gulp_components/tasks/ng-templates.js
+++ gulp_components/tasks/ng-templates.js
@@ -9,7 +9,7 @@ gulp.task('ng-templates',function(done){
 	opts.logMsg('\n*****' + 'begin ng-templates' + '*****\n');
 
 	//clean out the directory then make the new templates
-	opts.fs.emptyDir(opts.src + opts.jsDir + '/templates', function(err) {
+/*	opts.fs.emptyDir(opts.src + opts.jsDir + '/templates', function(err) {
 		gulp.src(opts.src + opts.ngTemplates)
 				.pipe(opts.plugins.ngHtml2js({
 					moduleName: "ibmnww",
@@ -17,5 +17,12 @@ gulp.task('ng-templates',function(done){
 				}))
 				.pipe(gulp.dest(opts.src + opts.jsDir + '/templates'));
 		done();
-	});
+	});*/
+
+	return 	gulp.src(opts.src + opts.ngTemplates)
+			.pipe(opts.plugins.ngHtml2js({
+				moduleName: "ibmnww",
+				declareModule: false
+			}))
+			.pipe(gulp.dest(opts.src + opts.jsDir + '/templates'));
 });
